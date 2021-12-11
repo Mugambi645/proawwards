9bd19fb0bd5070ca7c7aba7efb240af05af8f6d6
patrick mugambi <patrick@kali.pm.com>
2021-12-12 02:45:03 +0300

    register users

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+
+from django.urls import path,include
 from . import views
-app_name = "users"
+
+app_name = "users"   
+
+
 urlpatterns = [
-    
+    path("/", views.register, name="register"),
+
 ]
