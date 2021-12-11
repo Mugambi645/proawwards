f9d0dc326b38d0fb13b4431aa9e322e6026bbc1b
patrick mugambi <patrick@kali.pm.com>
2021-12-12 02:59:06 +0300

    create profile view logic

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse,reverse_lazy
 from users.forms import CustomUserCreationForm
 from django.views.decorators.csrf import csrf_protect
+from .models import Profile
 @csrf_protect
 def register(request):
     context = {}
@@ -16,3 +17,15 @@ def register(request):
     context['form']=form
     return render(request,'registration/register.html',context)
 
+def profile(request,id):
+    """[Profile]
+
+    Args:
+        request ([get]): [http method to retrieve data]
+        id ([profile]): [retrieve specified user profile]
+
+    Returns:
+        [type]: [description]
+    """
+    profile = Profile.objects.get(user = id)
+    return render(request,'profile.html',{"profile":profile})
