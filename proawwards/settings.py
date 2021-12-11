9be1e61ac811947fa4b1a398346c58c36258c8ea
patrick mugambi <patrick@kali.pm.com>
2021-12-11 13:44:43 +0300

    set postgres connection settings

--- proawwards/settings.py
+++ proawwards/settings.py
@@ -73,12 +73,17 @@ WSGI_APPLICATION = 'proawwards.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER':'patrick',
+        'PASSWORD':'flower2'
     }
 }
+
+
 
 
 # Password validation
