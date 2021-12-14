f02af77a6466df46081380844cdd07d315329355
patrick mugambi <patrick@kali.pm.com>
2021-12-14 11:36:43 +0300

    staticfiles input

--- proawwards/settings.py
+++ proawwards/settings.py
@@ -192,6 +192,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 #user management
 LOGIN_REDIRECT_URL = '/'
 
